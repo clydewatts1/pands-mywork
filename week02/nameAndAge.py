e62bbd3bbebbecadb5ab807e428e64f42b0b8233
Clyde Watts <Clyde.Watts267@gmail.com>
2025-02-04 21:57:05 +0000

    Did new task for week 2

---
+++ week02/nameAndAge.py
@@ -0,0 +1,9 @@
+#------------------------------------------------------------------------------
+# File   : nameAndAge.py
+# Author : Clyde Watts
+# Date   : 2025-02-0Clyde4
+# Purpose: Print the user's name and age to the console.
+#------------------------------------------------------------------------------
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+print(f"Hello {name}, \tyour age is {age}")
