ea59a31e12a950713618a424635cd3d9b07c4705
Clyde Watts <Clyde.Watts267@gmail.com>
2025-02-04 21:11:06 +0000

    Week 02 - my work

---
+++ week02/hello_world.py
@@ -0,0 +1,14 @@
+#------------------------------------------------------------------------------
+# File    : hello_world.py
+# Author  : Clyde Watts
+# Date    : 2025-02-04
+# Purpose : Print "Hello World" to the console.
+#------------------------------------------------------------------------------
+print("Hello World!")
+print("Hello Andrew!")
+answer = 111 * 555
+name = input("Please enter your name: ")
+print(f"Hello {name} \nNice to meet you")
+number = int(input("Please enter a number: "))
+new_number = number + 1
+print(f"The number you entered is {number} plus one is {new_number}")
