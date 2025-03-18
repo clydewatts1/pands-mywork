e8008146e80d4d7e84532ceb8df3a369dd4114cd
Clyde Watts <Clyde.Watts267@gmail.com>
2025-03-18 19:54:57 +0000

    did week08 plotting exercises

---
+++ week08-plotting/salaries.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+min_salary = 20000
+max_salary = 80000
+number_of_entries = 10
+# setup seed
+np.random.seed(42)
+salaries = np.random.randint(min_salary,max_salary,number_of_entries)
+
+print(salaries)
+
+# now add 5000 to each salary
+salaries_plus = salaries + 5000
+print(salaries_plus)
+
+# now add 5% to each salary
+salaries_plus5 = salaries * 1.05
+print(salaries_plus5)
+
+plt.hist(salaries, bins=100, color='blue')
+plt.show()
