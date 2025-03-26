7ca247326cdf75539475abfd61cbeeb6955e16e5
Clyde Watts <Clyde.Watts267@gmail.com>
2025-03-26 21:44:58 +0000

    Added more stuff to kmeans

---
+++ week09-errors/my_functions.py
@@ -0,0 +1,24 @@
+def fibonacci(number):
+    return []
+
+if __name__ == '__main__':
+    # test will go here
+    print("all good")
+    return7 = [0,1,1,2,3,5,8]
+    return11 = [0,1,1,2,3,5,8,13,21,34,55]
+    assert fibonacci(7) == return7, 'incorrect return for 7'
+    assert fibonacci(11) == return11, 'incorrect return for 11'
+    assert fibonacci(0) == [], 'incorrect return value for 0'
+    assert fibonacci(1) == [0], 'incorrect return value for 1'
+    try:
+        fibonacci(-1)
+    except ValueError:
+        # we want this exception to be thrown
+        # so this is an example where we want to do nothing
+        pass
+    else:
+        # if the exception was not thrown we should throw an AssertionError
+        assert False, 'fibonacci missing ValueError'
+        # or 
+        # raise AssertionError('fibonacci no value error')
+
